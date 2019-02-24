f9c5d2ab44d10baa1ba742143ca3cc0d882ee729
Maniac <remorse@csie.io>
2019-02-25 05:58:53 +0800

    initialize prototype `class FlickrImageCrop` for pytorch module `dataloader`

---
+++ vfn/datasets/FlickrImageCrop.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+# TODO:
+class FlickrImageCrop(Dataset):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
