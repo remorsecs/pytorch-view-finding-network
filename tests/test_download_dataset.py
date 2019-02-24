6c9fdff4f4b94d0d64d25f21428f68a2f5c0aa83
Maniac <remorse@csie.io>
2019-02-25 06:52:35 +0800

    add `test_image_is_equal_to_origin` and remove temp file with `finally`

--- tests/test_download_dataset.py
+++ tests/test_download_dataset.py
@@ -1,8 +1,11 @@
 import os
 
+
 from unittest import TestCase
 
-from download_dataset import download_an_image
+from PIL import Image, ImageChops
+
+from vfn.data.datasets.download_dataset import download_an_image
 
 
 class TestDownloadAnImage(TestCase):
@@ -13,5 +16,23 @@ class TestDownloadAnImage(TestCase):
         full_path = os.path.join(target_dir, filename)
 
         download_an_image(target_dir, url)
-        self.assertTrue(os.path.isfile(full_path))
-        os.remove(full_path)
+        try:
+            self.assertTrue(os.path.isfile(full_path))
+        finally:
+            os.remove(full_path)
+
+    def test_file_is_equal_to_origin(self):
+        url = 'https://farm6.staticflickr.com/5326/17704422191_400d428d48_c.jpg'
+        target_dir = './'
+        filename = url.split('/')[-1]
+        full_path = os.path.join(target_dir, filename)
+
+        download_an_image(target_dir, url)
+        images = [
+            Image.open(filename).convert('RGB'),
+            Image.open('test_' + filename).convert('RGB'),
+        ]
+        try:
+            self.assertIsNone(ImageChops.difference(*images).getbbox())
+        finally:
+            os.remove(full_path)
