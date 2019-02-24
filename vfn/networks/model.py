f45d43f3fb619b7030401a2285c000596d7e831b
Maniac <remorse@csie.io>
2019-02-25 07:34:56 +0800

    add `networks` package for network content (CNN-backbone, loss, network architecure, ...)

---
+++ vfn/networks/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class ViewFindingNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
