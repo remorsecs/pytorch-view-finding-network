ef7a3419d17e5f61a22089dcb249caa9fc85169b
Maniac <remorse@csie.io>
2019-02-26 05:51:15 +0800

    add `backbone` in vfn `__init__`

--- vfn/networks/models.py
+++ vfn/networks/models.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
+import vfn.networks.backbones
 
 
 class ViewFindingNet(nn.Module):
-    def __init__(self):
+    def __init__(self, backbone, use_spp=True,):
         super().__init__()
         pass
 
