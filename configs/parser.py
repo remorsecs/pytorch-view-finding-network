22d38f53f8029b35d6bbe23e59572b809b4f58c5
Maniac <remorse@csie.io>
2019-02-26 04:25:38 +0800

    add `ConfigParser` prototype for config file (yml) parsing

---
+++ configs/parser.py
@@ -0,0 +1,10 @@
+import yaml
+
+
+# parse `config.yml` for train, evaluation, infer.
+class ConfigParser(object):
+    def __init__(self, file):
+        pass
+
+    def parse(self) -> dict:
+        pass
