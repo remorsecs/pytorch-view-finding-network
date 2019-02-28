92491a7ba733c3607cce7959844e22e0c4676f63
Maniac <remorse@csie.io>
2019-03-01 07:55:34 +0800

    add `svm_loss` and `ranknet_loss`

---
+++ vfn/networks/losses.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+def svm_loss(x: torch.Tensor, y: torch.Tensor):
+    # Ref: https://pytorch.org/docs/stable/nn.html#marginrankingloss
+    criterion = nn.MarginRankingLoss(margin=1)
+    return criterion(x, y, torch.ones_like(x))
+
+
+def ranknet_loss(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    return nn.BCEWithLogitsLoss()(x, y)
