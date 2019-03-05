7230f51ccef4d545276f629e08efe94cda2c09c6
Yi-Ling Chen <puyenchild@gmail.com>
2019-03-04 23:24:30 -0800

    minor changes

--- vfn/data/datasets/FlickrPro.py
+++ vfn/data/datasets/FlickrPro.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import os
 import pickle
 from torch.utils.data import Dataset
-from tqdm import tqdm, trange
+from tqdm import trange
 
 import sys
 if sys.version_info[0] == 3:
@@ -14,14 +14,17 @@ else:
 
 
 class FlickrPro(Dataset):
+    __meta_name = 'flickr_pro.pkl'
+
     def __init__(self, root_dir, download=True):
         super(FlickrPro, self).__init__()
 
         self.root_dir = root_dir
+        self.meta_file = os.path.join(root_dir, self.__meta_name)
+
         if download:
             self._download(root_dir)
 
-        self.meta_file = os.path.join(root_dir, 'flickr_pro.pkl')
         self.img_list, self.annotations = self._fetch_metadata()
         self._check_integrity(root_dir)
 
@@ -37,10 +40,10 @@ class FlickrPro(Dataset):
             os.makedirs(root_dir)
 
         # download dataset.pkl to root_dir
-        if not os.path.exists(os.path.join(root_dir, 'flickr_pro.pkl')):
+        if not os.path.exists(self.meta_file):
             print('Downloading FlickrPro...')
             pkl_url = 'https://raw.githubusercontent.com/yiling-chen/view-finding-network/master/dataset.pkl'
-            urlretrieve(pkl_url, os.path.join(root_dir, 'flickr_pro.pkl'))
+            urlretrieve(pkl_url, self.meta_file)
             print('Done')
 
         # collect URLs and pass to ImageDownloader
