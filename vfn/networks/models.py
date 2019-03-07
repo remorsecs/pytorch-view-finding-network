6b2b8a5b6de3a2db097f3a133014fe6f05ffcd33
maniac <remorse@csie.io>
2019-03-08 04:22:48 +0800

    finish `ViewFindingNet` class

--- vfn/networks/models.py
+++ vfn/networks/models.py
@@ -1,12 +1,27 @@
+from typing import Type, TypeVar
+
 import torch
 import torch.nn as nn
-import vfn.networks.backbones
+from vfn.networks.backbones import Backbone
 
 
 class ViewFindingNet(nn.Module):
-    def __init__(self, backbone, use_spp=True,):
-        super().__init__()
-        pass
 
-    def forward(self, x):
-        pass
+    def __init__(self, backbone: Backbone):
+        super().__init__()
+        self.backbone = backbone
+        self.fc1 = nn.Sequential(
+            nn.Linear(self.backbone.output_dim(), 1000),
+            nn.ReLU(True),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(self.backbone.output_dim(), 1),
+        )
+
+    def forward(self, image):
+        x = self.backbone(image)    # type: torch.Tensor
+        x = x.view((x.size(0), -1))
+        x = self.fc1(x)
+        score = self.fc2(x)
+        return score
+
