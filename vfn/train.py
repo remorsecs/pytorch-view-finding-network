a26bbec864e9685e15387f83611e3673dce29006
maniac <remorse@csie.io>
2019-03-29 02:23:14 +0800

    add validation

--- vfn/train.py
+++ vfn/train.py
@@ -10,7 +10,7 @@ from configs.parser import ConfigParser
 
 def run(configs):
     device = configs.parse_device()
-    num_epochs = configs['train']['num_epochs']
+    num_epochs = configs.configs['train']['num_epochs']
     model_name = configs.get_model_name()
     model = configs.parse_model().to(device)
     data_loaders = configs.parse_dataloader()
@@ -60,15 +60,30 @@ def run(configs):
             score_C = model(image_crop)
 
             loss = loss_fn(score_I, score_C)
+            loss = loss.sum()
 
             return dict(
                 score_I=score_I,
                 score_C=score_C,
-                loss=loss.item()
+                sum_loss=loss.item()
             )
 
     evaluator = Engine(inference)
 
+    # for evaluator
+    @evaluator.on(Events.EPOCH_STARTED)
+    def init_sum_loss(engine):
+        engine.state.sum_loss = 0
+
+    @evaluator.on(Events.ITERATION_COMPLETED)
+    def compute_sum_loss(engine):
+        engine.state.sum_loss += engine.state.output['sum_loss']
+
+    @evaluator.on(Events.EPOCH_COMPLETED)
+    def compute_average_loss(engine):
+        engine.state.average_loss = engine.state.sum_loss / len(data_loaders['val'])
+
+    # for trainer
     @trainer.on(Events.ITERATION_COMPLETED)
     def log_training_loss(engine):
         iter = (engine.state.iteration - 1) % len(data_loaders['train']) + 1
@@ -82,21 +97,22 @@ def run(configs):
         pbar.refresh()
         tqdm.write(
             'Training Results - Epoch: {}\n'
-            'Loss: {:.4f}\n'.format(engine.state.epoch, engine.state.output['loss']))
+            'Loss: {:.4f}\n'.format(engine.state.epoch, engine.state.average_loss))
 
     ckpt_handler = ModelCheckpoint(
-        dirname=configs['checkpoint']['root_dir'],
-        filename_prefix=configs['checkpoint']['prefix'],
-        n_saved=configs['train']['num_epochs'],
+        dirname=configs.configs['checkpoint']['root_dir'],
+        filename_prefix=configs.configs['checkpoint']['prefix'],
+        save_interval=1,
+        n_saved=configs.configs['train']['num_epochs'],
     )
     trainer.add_event_handler(Events.EPOCH_COMPLETED, ckpt_handler, {model_name: model})
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_validation_results(engine):
         evaluator.run(data_loaders['val'])
-        # tqdm.write(
-        #     'Validation Results - Epoch: {}\n'
-        #     'Loss: {:.4f}\n'.format(engine.state.epoch, evaluator.state.output['loss']))
+        tqdm.write(
+            'Validation Results - Epoch: {}\n'
+            'Loss: {:.4f}\n'.format(engine.state.epoch, evaluator.state.average_loss))
 
         pbar.n = pbar.last_print_n = 0
 
