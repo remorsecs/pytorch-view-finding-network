89af14430739cbeade179f4107e6f94435aca1d2
Yi-Ling Chen <yilche@microsoft.com>
2020-05-07 23:52:56 -0700

    clean up unused codes

--- viewfinder_benchmark/data/FlickrPro.py
+++ viewfinder_benchmark/data/FlickrPro.py
@@ -7,24 +7,18 @@ import pickle
 from torch.utils.data import Dataset
 from tqdm import trange
 from viewfinder_benchmark.data.image_downloader import ImageDownloader
-from viewfinder_benchmark.data.ioutils import download
 
 
 class FlickrPro(Dataset):
-    # __meta_name = 'flickr_pro.pkl'
 
     def __init__(self, root_dir, meta_file, download=False):
         super(FlickrPro, self).__init__()
         self.root_dir = root_dir
-        # self.meta_file = os.path.join(root_dir, self.__meta_name)
         self.meta_file = meta_file
-        # self._download_metadata()
         self._fetch_metadata()
 
         if download:
             self._download_images()
-
-        self._check_integrity(root_dir)
 
     def __len__(self):
         return len(self.filenames)
@@ -34,17 +28,6 @@ class FlickrPro(Dataset):
 
     def get_all_items(self):
         return self.filenames, self.annotations, self.urls
-
-    def _download_metadata(self):
-        if not os.path.isdir(self.root_dir):
-            os.makedirs(self.root_dir)
-
-        # download dataset.pkl to root_dir
-        if not os.path.exists(self.meta_file):
-            print('Downloading FlickrPro...')
-            pkl_url = 'https://raw.githubusercontent.com/yiling-chen/view-finding-network/master/dataset.pkl'
-            download(pkl_url, self.meta_file)
-            print('Done')
 
     def _download_images(self):
         if not os.path.isdir(self.root_dir):
@@ -84,5 +67,7 @@ class FlickrPro(Dataset):
 
 if __name__ == "__main__":
     print(os.getcwd())
-    flickr_pro = FlickrPro("raw_images/flickr_pro", download=False)
+    flickr_pro = FlickrPro(root_dir="../../datasets/FlickrPro/src",
+                           meta_file="../../data/flickr_pro_train.pkl",
+                           download=False)
     print(flickr_pro[0])
